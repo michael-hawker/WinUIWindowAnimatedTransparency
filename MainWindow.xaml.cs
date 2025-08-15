a39ffb5a19207a50306afb33761ca4ab6fde8214
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2025-08-15 10:24:58 -0700

    Make Window Completely Transparent

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,18 @@
 using Microsoft.UI.Xaml;
+using WinUIEx;
 
 namespace WinUIExTransparent;
 
 /// <summary>
 /// An empty window that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class MainWindow : Window
+public sealed partial class MainWindow : WinUIEx.WindowEx
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        var hwnd = this.GetWindowHandle();
+        HwndExtensions.ToggleWindowStyle(hwnd, false, WindowStyle.TiledWindow);
     }
 }
